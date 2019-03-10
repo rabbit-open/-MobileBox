acd0ac5d316c01d514a67f1b987a05175326d9bf
B.S.Lee <254608684@qq.com>
2019-03-10 18:59:53 +0800

    设备扫描

--- bi/app/src/main/java/com/hualala/mobilebox/module/devices/DevicesListActivity.java
+++ bi/app/src/main/java/com/hualala/mobilebox/module/devices/DevicesListActivity.java
@@ -5,8 +5,11 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.widget.TextView;
 
 import com.hualala.bi.framework.base.BaseContractorActivity;
+import com.hualala.mobilebox.R;
 import com.hualala.ui.widget.PullLoadMoreView;
 
 import java.net.InetSocketAddress;
@@ -18,11 +21,21 @@ public class DevicesListActivity extends BaseContractorActivity {
 
     private PullLoadMoreView pullLoadMoreView;
 
+    private TextView result;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         pullLoadMoreView = new PullLoadMoreView(this);
         setContentView(pullLoadMoreView);
+
+        pullLoadMoreView.setContentView(LayoutInflater.from(this)
+                .inflate(R.layout.device_sacn_list, null));
+
+        result = findViewById(R.id.scanResult);
+
+
+        pullLoadMoreView.showLoading();
 
         initData();
         searchDevices_broadcast();
@@ -43,9 +56,22 @@ public class DevicesListActivity extends BaseContractorActivity {
     private Handler mHandler = new Handler(new Handler.Callback() {
         @Override
         public boolean handleMessage(Message msg) {
+
+            pullLoadMoreView.showContent();
+
             Log.v("tag", mDeviceList.get(0).room);
             Log.v("tag", mDeviceList.get(0).ip);
             Log.v("tag", mDeviceList.get(0).port + "");
+
+            String text = "";
+
+            for (int i = 0; i < mDeviceList.size(); i++) {
+                text += mDeviceList.get(i).getRoom() + " "
+                        + mDeviceList.get(i).getName() + " "
+                        + mDeviceList.get(i).getIp() + " "
+                        + mDeviceList.get(i).getPort() + "\n";
+            }
+            result.setText(text);
             return false;
         }
     });
