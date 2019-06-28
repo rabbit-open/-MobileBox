144dfbfe47a133d9e078add8708217f311d15111
B.S.Lee <254608684@qq.com>
2019-06-28 17:20:10 +0800

    测试全局loadView

--- shareAli/src/main/java/com/caidanmao/contract_package/apshare/Constants.java
+++ shareAli/src/main/java/com/caidanmao/contract_package/apshare/Constants.java
@@ -1,8 +1,5 @@
 package com.caidanmao.contract_package.apshare;
 
-/**
- * Created by linghan.wj on 2015/10/11.
- */
 public class Constants {
 
     private static final String APP_ID_ONLINE_OFFICIAL_SIGNATURE = "2019062865758129";
