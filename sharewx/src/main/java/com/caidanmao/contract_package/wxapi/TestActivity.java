86852b7cc3ee2b563eba85c7fc9e913742e5252a
B.S.Lee <254608684@qq.com>
2019-07-01 09:26:41 +0800

    测试全局loadView

--- sharewx/src/main/java/com/caidanmao/contract_package/wxapi/TestActivity.java
+++ sharewx/src/main/java/com/caidanmao/contract_package/wxapi/TestActivity.java
@@ -3,20 +3,30 @@ package com.caidanmao.contract_package.wxapi;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Environment;
+import android.view.View;
+
+import com.caidanmao.contract_package.R;
 
 import java.io.File;
 
-public class TestActivity extends Activity {
+public class TestActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        WeXinShareApi.shareLocalImageToSNS(getApplication(),
-                new File(Environment.getExternalStorageDirectory(), "share.jpg").getAbsolutePath(),
-                "", false);
-
-        WeXinShareApi.shareTextToSNS(getApplication(), "http://www.baidu.com", false);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.sharePic).setOnClickListener(this);
+        findViewById(R.id.shareText).setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.sharePic) {
+            WeXinShareApi.shareLocalImageToSNS(getApplication(),
+                    new File(Environment.getExternalStorageDirectory(), "share.jpg").getAbsolutePath(),
+                    "", false);
+        } else if (v.getId() == R.id.shareText) {
+            WeXinShareApi.shareTextToSNS(getApplication(), "http://www.baidu.com", false);
+        }
+    }
 }
