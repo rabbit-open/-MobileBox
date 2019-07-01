8f9f47763deab356a7977f5ff0196409db0a55ca
B.S.Lee <254608684@qq.com>
2019-07-01 09:19:17 +0800

    测试全局loadView

--- sharewx/src/main/java/com/caidanmao/contract_package/wxapi/KeyAndSecrets.java
+++ sharewx/src/main/java/com/caidanmao/contract_package/wxapi/KeyAndSecrets.java
@@ -2,7 +2,7 @@ package com.caidanmao.contract_package.wxapi;
 
 public interface KeyAndSecrets {
     // 微信APP ID
-    String WEIXINAPPID = "wx6f97138c4325187f";
+    String WEIXINAPPID = "wxc0e8cb41365e38b8";
     String WEIXIN_SCOPE = "snsapi_userinfo";
     String WEIXIN_APPSECRET = "";
     String WEIXIN_MERCHANT_ID = "";
